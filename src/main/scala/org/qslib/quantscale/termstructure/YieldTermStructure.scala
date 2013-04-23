d4bb67378e24b0a2095bca1bddce564c468b6a43
Choucri FAHED <choucri.fahed@mines-nancy.org>
2013-04-23 13:51:32 +0200

    Fix build

--- src/main/scala/org/qslib/quantscale/termstructure/YieldTermStructure.scala
+++ src/main/scala/org/qslib/quantscale/termstructure/YieldTermStructure.scala
@@ -20,8 +20,9 @@
  When applicable, the original copyright notice follows this notice.
  */
 /*
- Copyright (C) 2009 Ferdinando Ametrano
- Copyright (C) 2005 Joseph Wang
+ Copyright (C) 2004, 2009 Ferdinando Ametrano
+ Copyright (C) 2000, 2001, 2002, 2003 RiskMap srl
+ Copyright (C) 2003, 2004, 2005, 2006 StatPro Italia srl
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
@@ -37,34 +38,13 @@
  FOR A PARTICULAR PURPOSE.  See the license for more details.
 */
 
-package org.qslib.quantscale
+package org.qslib.quantscale.termstructure
 
-import org.qslib.quantscale.pattern.Observable
-import org.scala_tools.time.Imports._
-import org.qslib.quantscale.pattern.ObservableDefImpl
+import org.qslib.quantscale.TermStructure
 
 /**
- * Base class for events associated with a given date.
- * This class acts as a base class for the actual event implementations.
- *
- * @author Choucri FAHED
- * @since 1.0
+ * ==Interest-Rate Term Structure==
  */
-trait Event extends Observable with Ordered[Event] {
+trait YieldTermStructure extends TermStructure {
 
-  /** @return the date at which the event occurs */
-  def date(): LocalDate
-
-  /**
-   * @param includeRefDate If includeRefDate is true, then an event has not occurred if its
-   * date is the same as the refDate, i.e. this method returns false if
-   * the event date is the same as the refDate.
-   * @return true if an event has already occurred before a date
-   */
-  final def hasOccurred(refDate: LocalDate = LocalDate.now, includeRefDate: Boolean = false): Boolean =
-    if (includeRefDate) date < refDate else date <= refDate
-
-  @inline final def compare(that: Event): Int = date compare that.date
 }
-
-case class SimpleEvent(date: LocalDate) extends Event with ObservableDefImpl
