9f5a1a8040d216a9ffc6c5ecb83c19b1c97bdd4b
Choucri FAHED <choucri.fahed@mines-nancy.org>
2013-05-09 12:32:28 +0200

    LinearInterpolation

---
+++ src/main/scala/org/qslib/quantscale/math/interpolation/LinearInterpolation.scala
@@ -0,0 +1,82 @@
+/* 
+Copyright (C) 2013 Choucri FAHED
+
+ This source code is release under the BSD License.
+
+ This file is part of QuantScale, a free-software/open-source library
+ for financial quantitative analysts and developers - 
+ http://github.com/choucrifahed/quantscale
+
+ QuantScale is free software: you can redistribute it and/or modify it
+ under the terms of the QuantScale license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <choucri.fahed@mines-nancy.org>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+
+ QuantScale is based on QuantLib. http://quantlib.org/
+ When applicable, the original copyright notice follows this notice.
+ */
+/*
+ Copyright (C) 2000, 2001, 2002, 2003 RiskMap srl
+ Copyright (C) 2003, 2004, 2008 StatPro Italia srl
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+package org.qslib.quantscale.math.interpolation
+
+import org.saddle.Vec
+import org.qslib.quantscale._
+import scala.annotation.tailrec
+
+case class LinearInterpolation(xValues: Vec[Real], yValues: Vec[Real]) extends Interpolation {
+
+  @tailrec
+  private[this] def loop(sAcc: Vec[Real], primitiveConstAcc: Vec[Real], i: Int): (Vec[Real], Vec[Real]) = {
+    if (i == xValues.length) (sAcc, primitiveConstAcc)
+    else {
+      val dx = xValues.raw(i) - xValues.raw(i - 1)
+      val s = (yValues.raw(i) - yValues.raw(i - 1)) / dx
+      val primitiveConst = primitiveConstAcc.raw(i - 1) + dx * (yValues.raw(i - 1) + 0.5 * dx * s)
+      loop(sAcc concat Vec(s), primitiveConstAcc concat Vec(primitiveConst), i + 1)
+    }
+  }
+
+  private[this] val (s, primitiveConst) = loop(Vec(), Vec(0.0), 1)
+
+  protected override def value(x: Real): Real = {
+    val i = locate(x)
+    yValues.raw(i) + (x - xValues.raw(i)) * s.raw(i)
+  }
+
+  protected override def primitiveImpl(
+    x: Real, extrapolate: Boolean = false): Real = {
+    val i = locate(x)
+    val dx = x - xValues.raw(i)
+    primitiveConst.raw(i) + dx * (yValues.raw(i) + 0.5 * dx * s.raw(i))
+  }
+
+  protected override def derivativeImpl(
+    x: Real, extrapolate: Boolean = false): Real = s raw locate(x)
+
+  protected override def secondDerivativeImpl(
+    x: Real, extrapolate: Boolean = false): Real = 0.0
+}
+
+object LinearInterpolator extends Interpolator {
+  override final def interpolate(xValues: Vec[Real], yValues: Vec[Real]) = LinearInterpolation(xValues, yValues)
+}
